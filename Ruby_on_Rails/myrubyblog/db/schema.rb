24fd525cb5c69ceabf989065f3be87039b1c9a65
Milica Sucevic <milicasucevic@gmail.com>
2017-03-23 19:17:04 +0000

    Schema updated

--- Ruby_on_Rails/myrubyblog/db/schema.rb
+++ Ruby_on_Rails/myrubyblog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323191027) do
+ActiveRecord::Schema.define(version: 20170323191412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20170323191027) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "category_id"
+    t.integer  "author_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
