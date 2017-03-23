1ac0b0540e96e7598493e51632b7f34e20768fc1
Milica Sucevic <milicasucevic@gmail.com>
2017-03-23 18:57:07 +0000

    Views updated

--- Ruby_on_Rails/myrubyblog/app/controllers/posts_controller.rb
+++ Ruby_on_Rails/myrubyblog/app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 	def index
-
+		@content_first = "This is some sample text for our awesome new Ruby blog";
+		@content_second = "This is more sample text for our awesome new Ruby blog";
 	end
 
 	def new
