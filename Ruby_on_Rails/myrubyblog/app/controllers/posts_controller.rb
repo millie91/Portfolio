d5e77a8f26d56b7d4923a6394af2f1ca62a229be
Milica Sucevic <milicasucevic@gmail.com>
2017-03-26 14:20:26 +0100

    Code Updated

--- Ruby_on_Rails/myrubyblog/app/controllers/posts_controller.rb
+++ Ruby_on_Rails/myrubyblog/app/controllers/posts_controller.rb
@@ -8,23 +8,36 @@ class PostsController < ApplicationController
 	end
 
 	def new
-
+		@posts = Post.new
+		@category = Category.all
 	end
 
 	def create
-
+		@post = Post.new(params[:post])
+		if @post.save
+			redirect_to posts_path, :notice => "Your post has been saved"
+		else
+			render "new"
+		end
 	end
 
 	def edit
-		
+		@posts = Post.find(params[:id])
 	end
 
 	def update
-		
+		@posts = Post.find(params[:id])
+		if @posts.update_attributes(params[:post])
+			redirect_to post_path, :notice => "Your Post has been updated" 
+		else
+			render "edit"
+		end
 	end
 
 	def destroy
-		
+		@posts = Post.find(params[:id])
+		@posts.destroy
+		redirect_to posts_path, :notice => "Your Post has been deleted"
 	end
 
 	private
